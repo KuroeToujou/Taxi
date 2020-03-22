7887d24562b842cd68a399e3b5761f3e67cf00ab
Bagas <bagas.nugroho@amikom.ac.id>
2020-03-22 19:03:18 +0700

    inisialisasi project

---
+++ Properties.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace baru
+{
+    public class properties
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.Drivername = "Bambang";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+
+        }
+    }
+}
